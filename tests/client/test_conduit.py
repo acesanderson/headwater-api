0e3969898cdf42f329f256ec27322442a515c24b
Johnny Cakes <biden@phat.co.uk>
2025-10-23 22:16:34 -0500

    conduit client works like a dream!

--- tests/client/test_conduit.py
+++ tests/client/test_conduit.py
@@ -1,5 +1,11 @@
 from headwater_api.clients.conduit_client import ConduitClient
-from headwater_api.classes import ConduitRequest, ConduitResponse
+from headwater_api.classes import (
+    ConduitRequest,
+    ConduitResponse,
+    BatchRequest,
+    BatchResponse,
+    ConduitError,
+)
 from conduit.message.textmessage import TextMessage
 
 
@@ -12,3 +18,50 @@ def test_conduit_sync_client():
     assert isinstance(response, ConduitResponse)
     assert len(str(response.content)) > 0
     print(str(response.content))
+
+
+def test_conduit_async_service_prompt_strings():
+    # Create a sample BatchRequest
+    batch_request = BatchRequest(
+        model="gpt-oss:latest",
+        prompt_strings=[
+            "What is the capital of France?",
+            "Explain the theory of relativity.",
+            "Describe the process of photosynthesis.",
+        ],
+    )
+
+    # Call the conduit_async_service with the batch request
+    client = ConduitClient()
+    response = client.query_async(batch_request)
+
+    # Assert that the response is as expected
+    assert isinstance(response, BatchResponse)
+    assert isinstance(response.results, list)
+    assert len(response.results) == 3
+    assert not isinstance(response.results[0], ConduitError)
+    print(response)
+
+
+def test_conduit_async_service_input_variables():
+    # Create a sample BatchRequest with input variables
+    batch_request = BatchRequest(
+        model="gpt-oss:latest",
+        input_variables_list=[
+            {"things": "countries"},
+            {"things": "scientific theories"},
+            {"things": "biological processes"},
+            {"things": "programming languages"},
+        ],
+        prompt_str="Name 3 {things}.",
+    )
+
+    # Call the conduit_async_service with the batch request
+    client = ConduitClient()
+    response = client.query_async(batch_request)
+
+    # Assert that the response is as expected
+    assert isinstance(response, BatchResponse)
+    assert isinstance(response.results, list)
+    assert len(response.results) == 4
+    assert not isinstance(response.results[0], ConduitError)
